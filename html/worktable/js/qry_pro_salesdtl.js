59805eb1ab9e2a9b67d11654b48e194aacd1bd9c
heisadog <820753032@qq.com>
2018-03-27 15:26:37 +0800

    优化上传压缩 修改ios bug

--- html/worktable/js/qry_pro_salesdtl.js
+++ html/worktable/js/qry_pro_salesdtl.js
@@ -906,110 +906,48 @@ function dataSave(){
 function selectFileImage(fileObj) {
     var file = fileObj.files['0'];
     var agoimg=document.getElementById("ago");
-    console.log(file)
     //图片方向角 added by lzk
     var Orientation = null;
     if (file) {
-        console.log("正在上传,请稍后...");
         var rFilter = /^(image\/jpeg|image\/png)$/i; // 检查图片格式
         if (!rFilter.test(file.type)) {
-            //showMyTips("请选择jpeg、png格式的图片", false);
+            wfy.alert("请选择jpeg、png格式的图片");
             return;
         }
         // var URL = URL || webkitURL;
         //获取照片方向角属性，用户旋转控制
         EXIF.getData(file, function() {
-            //alert(EXIF.pretty(this));
             EXIF.getAllTags(this);
             Orientation = EXIF.getTag(this, 'Orientation');
-            //return;
         });
-
         var oReader = new FileReader();
         oReader.onload = function(e) {
-            //var blob = URL.createObjectURL(file);
-            //_compress(blob, file, basePath);
-            
             var image = new Image();
             image.src = e.target.result;
             agoimg.src = e.target.result;
             agoimg.onload = function() {
                 var expectWidth = this.naturalWidth;
                 var expectHeight = this.naturalHeight;
-                if (this.naturalWidth > this.naturalHeight && this.naturalWidth > 800) {
-                    expectWidth = 800;
-                    expectHeight = expectWidth * this.naturalHeight / this.naturalWidth;
-                } else if (this.naturalHeight > this.naturalWidth && this.naturalHeight > 1200) {
-                    expectHeight = 1200;
-                    expectWidth = expectHeight * this.naturalWidth / this.naturalHeight;
-                }
-                //expectWidth = 90;
+                var calc = expectWidth / expectHeight;
                 var canvas = document.getElementById('myCanvas');
                 var ctx = canvas.getContext("2d");
-                canvas.width = expectWidth;
-                canvas.height = expectHeight;
+                canvas.width = 1200;
+                canvas.height = (canvas.width)*calc;
                 console.log('canvas数据'+canvas.width)
-                ctx.drawImage(image, 0, 0, expectWidth, expectHeight);
                 var base64 = null;
                 //修复ios
-                if (navigator.userAgent.match(/iphone/i)) {
-                    console.log('iphone');
-                    //alert(expectWidth + ',' + expectHeight);
-                    //如果方向角不为1，都需要进行旋转 added by lzk
-                    if(Orientation != "" && Orientation != 1){
-                        //alert('旋转处理');
-                        switch(Orientation){
-                            case 6://需要顺时针（向左）90度旋转
-                                //alert('需要顺时针（向左）90度旋转');
-                                rotateImg(this,'left',canvas);
-                                break;
-                            case 8://需要逆时针（向右）90度旋转
-                                //alert('需要顺时针（向右）90度旋转');
-                                rotateImg(this,'right',canvas);
-                                break;
-                            case 3://需要180度旋转
-                                //alert('需要180度旋转');
-                                rotateImg(this,'right',canvas);//转两次
-                                rotateImg(this,'right',canvas);
-                                break;
-                        }
-                    }
-
-                    /*var mpImg = new MegaPixImage(image);
-                     mpImg.render(canvas, {
-                     maxWidth: 800,
-                     maxHeight: 1200,
-                     quality: 0.8,
-                     orientation: 8
-                     });*/
-                    base64 = canvas.toDataURL("image/jpeg", 0.1);
-                }else if (navigator.userAgent.match(/Android/i)) {// 修复android
-                    var encoder = new JPEGEncoder();
-                    base64 = encoder.encode(ctx.getImageData(0, 0, expectWidth, expectHeight), 80);
-                }else{
-                    //alert(Orientation);
-                    if(Orientation != "" && Orientation != 1){
-                        //alert('旋转处理');
-                        switch(Orientation){
-                            case 6://需要顺时针（向左）90度旋转
-                                //alert('需要顺时针（向左）90度旋转');
-                                rotateImg(this,'left',canvas);
-                                break;
-                            case 8://需要逆时针（向右）90度旋转
-                                //alert('需要顺时针（向右）90度旋转');
-                                rotateImg(this,'right',canvas);
-                                break;
-                            case 3://需要180度旋转
-                                //alert('需要180度旋转');
-                                rotateImg(this,'right',canvas);//转两次
-                                rotateImg(this,'right',canvas);
-                                break;
-                        }
-                    }
-
-                    base64 = canvas.toDataURL("image/jpeg", 0.1);
+                if (Orientation == 6) {
+                    //alert('需要顺时针（向左）90度旋转');
+                    ctx.save(); //保存状态
+                    ctx.translate(canvas.width/2, canvas.height/2); //设置画布上的(0,0)位置，也就是旋转的中心点
+                    ctx.rotate(90 * Math.PI / 180); //把画布旋转90度
+                    // 执行Canvas的drawImage语句
+                    ctx.drawImage(image, -(canvas.width/2), -(canvas.height/2), canvas.width, canvas.height); //把图片绘制在画布translate之前的中心点，
+                    ctx.restore(); //恢复状态
+                }else {
+                    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
                 }
-                //uploadImage(base64);
+                base64 = canvas.toDataURL("image/jpeg", 0.92);
                 $("#check_img").attr("src", base64);
             };
         };
@@ -1018,70 +956,6 @@ function selectFileImage(fileObj) {
     //$('#uploadImage').imgloadsize();
 
 }
-
-//对图片旋转处理 added by lzk
-function rotateImg(img, direction,canvas) {
-    //alert(img);
-    //最小与最大旋转方向，图片旋转4次后回到原方向
-    var min_step = 0;
-    var max_step = 3;
-    //var img = document.getElementById(pid);
-    if (img == null)return;
-    //img的高度和宽度不能在img元素隐藏后获取，否则会出错
-    var height = img.height;
-    var width = img.width;
-    var calc = width/height;
-    //var step = img.getAttribute('step');
-    var step = 2;
-    if (step == null) {
-        step = min_step;
-    }
-    if (direction == 'right') {
-        step++;
-        //旋转到原位置，即超过最大值
-        step > max_step && (step = min_step);
-    } else {
-        step--;
-        step < min_step && (step = max_step);
-    }
-    //img.setAttribute('step', step);
-    /*var canvas = document.getElementById('pic_' + pid);
-     if (canvas == null) {
-     img.style.display = 'none';
-     canvas = document.createElement('canvas');
-     canvas.setAttribute('id', 'pic_' + pid);
-     img.parentNode.appendChild(canvas);
-     }  */
-    //旋转角度以弧度值为参数
-    var degree = step * 90 * Math.PI / 180;
-    var ctx = canvas.getContext('2d');
-    console.log('step是'+step)
-    switch (step) {
-        case 0:
-            canvas.width = width;
-            canvas.height = height;
-            ctx.drawImage(img, 0, 0);
-            break;
-        case 1:
-            canvas.width = height;
-            canvas.height = width;
-            ctx.rotate(degree);
-            ctx.drawImage(img, 0, -height );
-            break;
-        case 2:
-            canvas.width = width;
-            canvas.height = height;
-            ctx.rotate(degree);
-            ctx.drawImage(img, -width, -height);
-            break;
-        case 3:
-            canvas.width = height;
-            canvas.height = width;
-            ctx.rotate(degree);
-            ctx.drawImage(img, -width, 0);
-            break;
-    }
-}    
 
 
 
